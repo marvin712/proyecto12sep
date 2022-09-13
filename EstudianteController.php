a80e8a5b74ea429fa9cd224a9474b38b018f9c32
marvin712 <62572934+marvin712@users.noreply.github.com>
2022-09-12 20:56:24 -0600

    Update EstudianteController.php

--- EstudianteController.php
+++ EstudianteController.php
@@ -6,19 +6,23 @@ $nuevoEstudiante = new Estudiante();
 if(isset($_POST['btnGuardar']))
 {
     $nuevoEstudiante->GuardarEstudiante($_POST['apellidos'],$_POST['nombre'],$_POST['direccion'],$_POST['telefono']);
+     header('Location: vistaEstudiante.php');
+
 }
 /*EDITAR*/
 else
 if(isset($_POST['btnEditar']))
 {
  $nuevoEstudiante->EditarEstudiante($_POST['idStudent'],$_POST['txtapellidos'],$_POST['txtnombre'],$_POST['txtdireccion'],$_POST['txttelefono']); 
-        
+         header('Location: vistaEstudiante.php');
+
 }
 
 else
 if(isset($_POST['btnEliminar']))
 {
  $nuevoEstudiante->EliminarEstudiante($_POST['idStudent'],$_POST['txtapellidos'],$_POST['txtnombre'],$_POST['txtdireccion'],$_POST['txttelefono']); 
-        
+         header('Location: vistaEstudiante.php');
+
 }
 
